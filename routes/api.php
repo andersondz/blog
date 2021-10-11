6a962cd20680000fb2bebf14b20eccf9f078e4af
ander96 <anderson9daza6@gmail.com>
2021-10-11 17:50:26 -0500

    #6 Rutas para article

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Article;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('articles', function () {
+    return Article::all();
+});
+
+Route::get('articles/{id}', function ($id) {
+    return Article::find($id);
+});
+
+Route::post('articles', function (Request $request) {
+    return Article::create($request->all());
+});
+
+Route::put('articles/{id}', function (Request $request, $id) {
+    $article = Article::findOrFail($id);
+    $article->update($request->all());
+    return $article;
+});
+
+Route::delete('articles/{id}', function ($id) {
+
+    $article = Article::find($id);
+    $title = $article->title;
+    $article->delete();
+    return "Articulo ".$title.", se ha eleminado";
 });
